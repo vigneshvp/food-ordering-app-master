50fb689d66c36816cb484079a7bfac313029eeb9
vigneshvp <viki3110@gmail.com>
2021-05-02 13:22:54 +0530

    Added Fast Food icon

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
+import FastfoodIcon from '@material-ui/icons/Fastfood';
 
 class Header extends Component {
     render() {
         return (
             <div>
-                <Button variant="contained" color="default">
-                    Login
-                </Button>
+                <header className="app-header">
+                    <FastfoodIcon className="icon"/>
+                    <div className="login-button">
+                        <Button variant="contained" color="default">
+                            Login
+                        </Button>
+                    </div>
+                </header>
             </div>
         )
     }
