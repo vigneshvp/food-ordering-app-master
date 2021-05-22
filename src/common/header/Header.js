868bf844579a4f8ddaba7d00fe7bbf621bcabd1b
vigneshvp <viki3110@gmail.com>
2021-05-22 15:46:55 +0530

    Added a modal with 2 tabs for Login and Register

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -4,10 +4,33 @@ import Button from '@material-ui/core/Button';
 import FastfoodIcon from '@material-ui/icons/Fastfood';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import {IconButton,InputAdornment, TextField} from "@material-ui/core";
+import {IconButton, InputAdornment, Tab, Tabs, TextField} from "@material-ui/core";
+import Modal from 'react-modal';
 
 
 class Header extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            modalIsOpen: false,
+            value: 0
+        }
+    }
+
+    openModalHandler = () => {
+        this.setState({ modalIsOpen: true });
+    }
+
+    closeModalHandler = () => {
+        this.setState({ modalIsOpen: false });
+    }
+
+    tabChangeHandler = (event, value) => {
+        this.setState({ value });
+    }
+
+
     render() {
         const styles = {
             color:'white',
@@ -32,12 +55,22 @@ class Header extends Component {
                         }}
                     />
                     <div className="login-button">
-                        <Button variant="contained" color="default">
+                        <Button variant="contained" color="default" onClick={this.openModalHandler}>
                             <AccountCircleIcon/>
                             Login
                         </Button>
                     </div>
                 </header>
+                <Modal
+                    ariaHideApp={false}
+                    isOpen={this.state.modalIsOpen}
+                    contentLabel="Login"
+                    onRequestClose={this.closeModalHandler}>
+                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                        <Tab label="Login" />
+                        <Tab label="Register" />
+                    </Tabs>
+                </Modal>
             </div>
         )
     }
